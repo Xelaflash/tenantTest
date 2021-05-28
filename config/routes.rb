4c8576a9b1ce96bbc3e7a8de47d0aba722c77d8b
Alexandre Gros <xelaflash@gmail.com>
2021-05-28 07:25:55 -0400

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   constraints subdomain: /.*/ do
     resources :articles
   end
+
+  root to: "authors#index"
 
   resources :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
